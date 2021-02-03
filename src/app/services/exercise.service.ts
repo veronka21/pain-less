d81316f3efbba8193325ca0e6581a45e025d48ac
Veronika Takács <roni9764@gmail.com>
2021-02-03 15:37:53 +0100

    Add more sample exercises

--- src/app/services/exercise.service.ts
+++ src/app/services/exercise.service.ts
@@ -3,7 +3,19 @@ import { Exercise } from "../shared/exercise.model";
 export class ExerciseService {
     private sampleExercises : Exercise[] = [
         new Exercise(1, 'Sample Exercise', 'Sample Description', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise2', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif')
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
+        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
     ];
 
     getExercises() {
