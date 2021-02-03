c1a8fbcd3d08b77f433015343170024ad1358b01
Veronika Takács <roni9764@gmail.com>
2021-02-03 10:24:33 +0100

    Add routing, register app routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { ExerciseLibraryComponent } from './exercise-library/exercise-library.component';
+import { ExerciseListComponent } from './exercise-library/exercise-list/exercise-list.component';
+import { ExerciseComponent } from './exercise-library/exercise-list/exercise/exercise.component';
+
+const appRoutes : Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'exercise-library', component: ExerciseLibraryComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ExerciseLibraryComponent,
+    ExerciseListComponent,
+    ExerciseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
