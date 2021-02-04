2eb2475d53d4fa52309126580786cece49b79cd2
Takács Veronika <roni9764@gmail.com>
2021-02-04 16:22:22 +0100

    Remove sample exercises and add method to get exercises from backend url

--- src/app/services/exercise.service.ts
+++ src/app/services/exercise.service.ts
@@ -1,24 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
 import { Exercise } from "../shared/exercise.model";
 
+@Injectable()
 export class ExerciseService {
-    private sampleExercises : Exercise[] = [
-        new Exercise(1, 'Sample Exercise', 'Sample Description', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-        new Exercise(2, 'Sample Exercise with very long name', 'Sample Description2', 'https://coveteur.com/wp-content/uploads/2015/12/erika_bloom-1.gif'),
-    ];
+    private apiMainUrl : string = "http://localhost:8762/exercise"
 
-    getExercises() {
-        return this.sampleExercises;
+    constructor(private http: HttpClient) {}
+    
+    getAllExercisesFromApi() : Observable<Exercise[]> {
+        return this.http.get<Exercise[]>(`${this.apiMainUrl}/all`)
     }
 }
