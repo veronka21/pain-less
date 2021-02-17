2af716cbecca55c6bf6cd9ec40e9df5b05c2b5fd
Veronika Takács <roni9764@gmail.com>
2021-02-17 14:39:24 +0100

    Add WorkoutPlanDetailsComponent

---
+++ src/app/workout-plans/workout-plan-details/workout-plan-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-plan-details',
+  templateUrl: './workout-plan-details.component.html',
+  styleUrls: ['./workout-plan-details.component.scss']
+})
+export class WorkoutPlanDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
