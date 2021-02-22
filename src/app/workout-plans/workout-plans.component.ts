69830897850d1321cd6d9e9781e83144b46ed1d1
Takács Veronika <roni9764@gmail.com>
2021-02-22 23:35:58 +0100

    Get workout plans from backend url

--- src/app/workout-plans/workout-plans.component.ts
+++ src/app/workout-plans/workout-plans.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { WorkoutPlanService } from '../services/workout-plan.service';
+import { Workout } from '../shared/workout.model';
 
 @Component({
   selector: 'app-workout-plans',
   templateUrl: './workout-plans.component.html',
-  styleUrls: ['./workout-plans.component.scss']
+  styleUrls: ['./workout-plans.component.scss'],
+  providers: [WorkoutPlanService]
 })
-export class WorkoutPlansComponent implements OnInit {
+export class WorkoutPlansComponent implements OnInit, OnDestroy {
+  workouts: Workout[] = [];
+  selectedWorkout?: Workout;
+  loading: boolean = true;
+  subscriptions: Subscription = new Subscription();
 
-  constructor() { }
-
+  constructor(private workoutPlanService: WorkoutPlanService) { }
+  
   ngOnInit(): void {
+    this.subscriptions = this.workoutPlanService.getAllWorkoutsFromApi()
+    .subscribe(response => {
+      this.loading = false;
+      this.workouts = response;
+    }, () => this.loading = false)
   }
-
+  
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
