840478429cd02b27e0323a4644632a4a228404cd
Takács Veronika <roni9764@gmail.com>
2021-02-22 23:33:05 +0100

    Modify property name

--- src/app/shared/exercise.model.ts
+++ src/app/shared/exercise.model.ts
@@ -1,5 +1,5 @@
 export class Exercise {
-    public length: number = 0;
+    public lengthInSeconds: number = 0;
 
     constructor(public id: string, public name: string, public description: string, public gifUrl: string) {}
 }
