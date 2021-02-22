df2b52b86e12ff4cdecd0a9cc00f53277f8c5949
Takács Veronika <roni9764@gmail.com>
2021-02-22 23:34:48 +0100

    Handle subscriptions

--- src/app/exercise-library/exercise-list/exercise-list.component.ts
+++ src/app/exercise-library/exercise-list/exercise-list.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ExerciseService } from 'src/app/services/exercise.service';
 import { Exercise } from 'src/app/shared/exercise.model';
 import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { Subscription, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-exercise-list',
   templateUrl: './exercise-list.component.html',
   styleUrls: ['./exercise-list.component.css']
 })
-export class ExerciseListComponent implements OnInit {
+export class ExerciseListComponent implements OnInit, OnDestroy {
   exercises: Exercise[] = [];
   loading: boolean = true;
+  subscriptions: Subscription = new Subscription();
 
   constructor(private exerciseService: ExerciseService) { 
   }
-
+  
   ngOnInit(): void {
-    this.exerciseService.getAllExercisesFromApi()
-      .subscribe(response => {
-        this.loading = false;
-        this.exercises = response;
-      }, () => this.loading = false);
+    this.subscriptions = this.exerciseService.getAllExercisesFromApi()
+    .subscribe(response => {
+      this.loading = false;
+      this.exercises = response;
+    }, () => this.loading = false);
+  }
+  
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
